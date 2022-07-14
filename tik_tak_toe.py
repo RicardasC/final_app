0b42f21597ccf6a43850e7b358284ef50d4685ae
RicardasC <106992701+RicardasC@users.noreply.github.com>
2022-07-14 14:28:01 +0200

    Update tik_tak_toe.py

--- tik_tak_toe.py
+++ tik_tak_toe.py
@@ -1,107 +1,130 @@
-import tkinter as tk
+from tkinter import * 
 from tkinter import messagebox
 
-window = tk.Tk()
-window.title("Tik Tak Toe")
+window = Tk()
+window.title('Tik Tak Toe')
 window.geometry('623x651')
+
 
 clicked = True
 count = 0
 
-def winner():
+def finalwinner():
+    global winner
+    winner = False
 
     if b1["text"] == "X" and b2["text"] == "X" and b3["text"] == "X":
         b1.config(bg="green"), b2.config(bg="green"), b3.config(bg="green")
-        messagebox.showinfo("Tik Tak Toe", "X Laimėjote!")
-   
+        winner = True
+        messagebox.askyesno("X Laimėjo", "Ar bandysite dar kartą?")
+
     elif b4["text"] == "X" and b5["text"] == "X" and b6["text"] == "X":
         b4.config(bg="green"), b5.config(bg="green"), b6.config(bg="green")
-        messagebox.showinfo("Tik Tak Toe", "X Laimėjote!")
+        winner = True
+        messagebox.askyesno("X Laimėjo", "Ar bandysite dar kartą?")
         
     elif b7["text"] == "X" and b8["text"] == "X" and b9["text"] == "X":
         b7.config(bg="green"), b8.config(bg="green"), b9.config(bg="green")
-        messagebox.showinfo("Tik Tak Toe", "X Laimėjote!")
+        winner = True
+        messagebox.askyesno("X Laimėjo", "Ar bandysite dar kartą?")
 
     elif b1["text"] == "X" and b4["text"] == "X" and b7["text"] == "X":
         b1.config(bg="green"), b4.config(bg="green"), b7.config(bg="green")
-        messagebox.showinfo("Tik Tak Toe", "X Laimėjote!")
+        winner = True
+        messagebox.askyesno("X Laimėjo", "Ar bandysite dar kartą?")
 
     elif b2["text"] == "X" and b5["text"] == "X" and b8["text"] == "X":
         b2.config(bg="green"), b5.config(bg="green"), b8.config(bg="green")
-        messagebox.showinfo("Tik Tak Toe", "X Laimėjote!")
+        winner = True
+        messagebox.askyesno("X Laimėjo", "Ar bandysite dar kartą?")
 
     elif b3["text"] == "X" and b6["text"] == "X" and b9["text"] == "X":
         b3.config(bg="green"), b6.config(bg="green"), b9.config(bg="green")
-        messagebox.showinfo("Tik Tak Toe", "X Laimėjote!")     
+        winner = True
+        messagebox.askyesno("X Laimėjo", "Ar bandysite dar kartą?")
 
     elif b1["text"] == "X" and b5["text"] == "X" and b9["text"] == "X":
         b1.config(bg="green"), b5.config(bg="green"), b9.config(bg="green")
-        messagebox.showinfo("Tik Tak Toe", "X Laimėjote!")
+        winner = True
+        messagebox.askyesno("X Laimėjo", "Ar bandysite dar kartą?")
 
     elif b3["text"] == "X" and b5["text"] == "X" and b7["text"] == "X":
         b3.config(bg="green"), b5.config(bg="green"), b7.config(bg="green")
-        messagebox.showinfo("Tik Tak Toe", "X Laimėjote!")
-     
+        winner = True
+        messagebox.askyesno("X Laimėjo", "Ar bandysite dar kartą?")
+
     elif b1["text"] == "O" and b2["text"] == "O" and b3["text"] == "O":
         b1.config(bg="green"), b2.config(bg="green"), b3.config(bg="green")
-        messagebox.showinfo("Tik Tak Toe", "O Laimėjote!")
-  
+        winner = True
+        messagebox.askyesno("O Laimėjo", "Ar bandysite dar kartą?")
+
     elif b4["text"] == "O" and b5["text"] == "O" and b6["text"] == "O":
         b4.config(bg="green"), b5.config(bg="green"), b6.config(bg="green")
-        messagebox.showinfo("Tik Tak Toe", "O Laimėjote!")
-     
+        winner = True
+        messagebox.askyesno("O Laimėjo", "Ar bandysite dar kartą?")
+        
     elif b7["text"] == "O" and b8["text"] == "O" and b9["text"] == "O":
         b7.config(bg="green"), b8.config(bg="green"), b9.config(bg="green")
-        messagebox.showinfo("Tik Tak Toe", "O Laimėjote!") 
+        winner = True
+        messagebox.askyesno("O Laimėjo", "Ar bandysite dar kartą?")
 
     elif b1["text"] == "O" and b4["text"] == "O" and b7["text"] == "O":
         b1.config(bg="green"), b4.config(bg="green"), b7.config(bg="green")
-        messagebox.showinfo("Tik Tak Toe", "O Laimėjote!")
-   
+        winner = True
+        messagebox.askyesno("O Laimėjo", "Ar bandysite dar kartą?")
+
     elif b2["text"] == "O" and b5["text"] == "O" and b8["text"] == "O":
         b2.config(bg="green"), b5.config(bg="green"), b8.config(bg="green")
-        messagebox.showinfo("Tik Tak Toe", "O Laimėjote!")
- 
+        winner = True
+        messagebox.askyesno("O Laimėjo", "Ar bandysite dar kartą?")
+
     elif b3["text"] == "O" and b6["text"] == "O" and b9["text"] == "O":
         b3.config(bg="green"), b6.config(bg="green"), b9.config(bg="green")
-        messagebox.showinfo("Tik Tak Toe", "O Laimėjote!")
+        winner = True
+        messagebox.askyesno("O Laimėjo", "Ar bandysite dar kartą?")
 
     elif b1["text"] == "O" and b5["text"] == "O" and b9["text"] == "O":
         b1.config(bg="green"), b5.config(bg="green"), b9.config(bg="green")
-        messagebox.showinfo("Tik Tak Toe", "O Laimėjote!")
-  
+        winner = True
+        messagebox.askyesno("O Laimėjo", "Ar bandysite dar kartą?")
+
     elif b3["text"] == "O" and b5["text"] == "O" and b7["text"] == "O":
         b3.config(bg="green"), b5.config(bg="green"), b7.config(bg="green")
-        messagebox.showinfo("Tik Tak Toe", "O Laimėjote!")
+        winner = True
+        messagebox.askyesno("O Laimėjo", "Ar bandysite dar kartą?")
 
-    elif count == 9:
-        messagebox.showinfo("Tik Tak Toe", "Lygiosios")
-   
+    if count == 9 and winner == False:
+        root.configure(bg='blue')
+        messagebox.askyesno("Lygiosios", "Ar bandysite dar kartą?")
+
 def b_click(b):
     global clicked, count
-    if b["text"] == " " and clicked ==True:
+
+    if b["text"] == " " and clicked == True:
         b["text"] = "X"
         clicked = False
-        count = count + 1
-        winner()
-    if b["text"] == " " and clicked == False:
+        count += 1
+        finalwinner()
+    elif b["text"] == " " and clicked == False:
         b["text"] = "O"
         clicked = True
-        count = count + 1
-        winner()
+        count += 1
+        finalwinner()
+    else:
+        messagebox.showerror("Tik Tak Toe", "Langelis užimtas, bandykite kitą")
 
-b1 = tk.Button(text= " ", font=("arial", 80, "bold"), height=1, width=3, relief="sunken", bg="cornflowerblue", command = lambda: b_click(b1))
-b2 = tk.Button(text= " ", font=("arial", 80, "bold"), height=1, width=3, bg="lightgoldenrod1", command = lambda: b_click(b2))
-b3 = tk.Button(text= " ", font=("arial", 80, "bold"), height=1, width=3, relief="sunken", bg="cornflowerblue", command = lambda: b_click(b3))
+b1 = Button(window, text= " ", font=("arial", 80, "bold"), height=1, width=3, relief="sunken", bg="cornflowerblue", command = lambda: b_click(b1))
+b2 = Button(window, text= " ", font=("arial", 80, "bold"), height=1, width=3, bg="lightgoldenrod1", command = lambda: b_click(b2))
+b3 = Button(window, text= " ", font=("arial", 80, "bold"), height=1, width=3, relief="sunken", bg="cornflowerblue", command = lambda: b_click(b3))
 
-b4 = tk.Button(text= " ", font=("arial", 80, "bold"), height=1, width=3, bg="lightgoldenrod1", command = lambda: b_click(b4))
-b5 = tk.Button(text= " ", font=("arial", 80, "bold"), height=1, width=3, relief="sunken", bg="cornflowerblue", command = lambda: b_click(b5))
-b6 = tk.Button(text= " ", font=("arial", 80, "bold"), height=1, width=3, bg="lightgoldenrod1", command = lambda: b_click(b6))
+b4 = Button(window, text= " ", font=("arial", 80, "bold"), height=1, width=3, bg="lightgoldenrod1", command = lambda: b_click(b4))
+b5 = Button(window, text= " ", font=("arial", 80, "bold"), height=1, width=3, relief="sunken", bg="cornflowerblue", command = lambda: b_click(b5))
+b6 = Button(window, text= " ", font=("arial", 80, "bold"), height=1, width=3, bg="lightgoldenrod1", command = lambda: b_click(b6))
 
-b7 = tk.Button(text= " ", font=("arial", 80, "bold"), height=1, width=3, relief="sunken", bg="cornflowerblue", command = lambda: b_click(b7))
-b8 = tk.Button(text= " ", font=("arial", 80, "bold"), height=1, width=3, bg="lightgoldenrod1", command = lambda: b_click(b8))
-b9 = tk.Button(text= " ", font=("arial", 80, "bold"), height=1, width=3, relief="sunken", bg="cornflowerblue", command = lambda: b_click(b9))
-            
+b7 = Button(window, text= " ", font=("arial", 80, "bold"), height=1, width=3, relief="sunken", bg="cornflowerblue", command = lambda: b_click(b7))
+b8 = Button(window, text= " ", font=("arial", 80, "bold"), height=1, width=3, bg="lightgoldenrod1", command = lambda: b_click(b8))
+b9 = Button(window, text= " ", font=("arial", 80, "bold"), height=1, width=3, relief="sunken", bg="cornflowerblue", command = lambda: b_click(b9))
+
 b1.grid(row=0, column=0)
 b2.grid(row=0, column=1)
 b3.grid(row=0, column=2)
